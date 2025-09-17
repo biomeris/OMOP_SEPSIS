dd75502dd1d6afc075b6ef04d840c8098f839b9b
Vittoria Ramella <108271982+vittoriaramella@users.noreply.github.com>
2025-09-17 16:19:03 +0200

    Update estrazione_covariate.sql

--- [OMOP_SEPSIS]-PredictorVariables/sql/estrazione_covariate.sql
+++ [OMOP_SEPSIS]-PredictorVariables/sql/estrazione_covariate.sql
@@ -2440,7 +2440,7 @@ FROM
 			JOIN #ricoveri_tmp r ON m.person_id = r.person_id
 			AND m.measurement_date = r.visit_start_date
 		WHERE
-			m.measurement_concept_id IN (4118986,3024128,40757494) -- Bilirubin measurement
+			m.measurement_concept_id IN (4118986,3024128,40757494,3007242,4216632) -- Bilirubin measurement
 	) AS all_bilirubina_base
 WHERE
 	all_bilirubina_base.rank = 1;
@@ -2473,7 +2473,7 @@ FROM
 			AND m.measurement_date <= r.visit_end_date
 			AND m.measurement_date <= (r.visit_start_date + 3)
 		WHERE
-			m.measurement_concept_id IN (4118986,3024128,40757494) -- Bilirubin measurement
+			m.measurement_concept_id IN (4118986,3024128,40757494,3007242,4216632) -- Bilirubin measurement
 	) AS all_bilirubina_base
 WHERE
 	all_bilirubina_base.rank = 1;
