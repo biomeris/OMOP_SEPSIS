f779d96233241e76e732392c77073dd108695dea
Vittoria Ramella <108271982+vittoriaramella@users.noreply.github.com>
2026-01-13 13:48:18 +0100

    Bug fix gruppi-id9cm.sql

--- [OMOP_SEPSIS]-PredictorVariables/sql/gruppi-id9cm.sql
+++ [OMOP_SEPSIS]-PredictorVariables/sql/gruppi-id9cm.sql
@@ -84,7 +84,7 @@ SELECT
 	all_diag.icd9_group,
 	all_diag.icd9_group_name,
 	COUNT(*) AS n_diagnosi
-INTO @results_schema.omop_sepsis_icd9_diagnosis_cases
+INTO @results_schema.omop_sepsis_icd9_diagnosis
 FROM (
 	SELECT DISTINCT
 		r.visit_occurrence_id,
